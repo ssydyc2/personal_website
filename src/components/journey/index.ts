65f4bd09d41f22c84a9d4733a4850e2235848d2b
Siyuan Song <ssydyc2@gmail.com>
2026-02-06 22:02:56 -0800

    Refactor Journey.tsx into smaller components
    - Extract GPUProgrammingContent (Triton + JAX) to separate file
    - Extract AIPerformanceContent to separate file
    - Extract PapersContent to separate file
    - Journey.tsx reduced from 389 lines to 66 lines
    - All components now in src/components/journey/

---
+++ src/components/journey/index.ts
@@ -0,0 +1,3 @@
+export { default as GPUProgrammingContent } from './GPUProgrammingContent';
+export { default as AIPerformanceContent } from './AIPerformanceContent';
+export { default as PapersContent } from './PapersContent';
